82c34cc8b94666621f4c2a7b05d8f3cdc96a7f8b
Michael D. Hoyle <hoylemd@gmail.com>
2015-04-05 14:21:49 -0400

    initial AST node definitions

---
+++ parser.cpp
@@ -0,0 +1,13 @@
+#include "parser.h"
+
+BinaryOperationExpression::~BinaryOperationExpression () {
+  delete leftOperand;
+  delete rightOperand;
+}
+
+InvocationExpression::~InvocationExpression () {
+  int i=0, size = arguments.size();
+  for (i=size - 1; i >= 0; i -= 1) {
+    delete arguments[i];
+  }
+}
